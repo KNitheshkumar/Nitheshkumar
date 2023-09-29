e9ae193620fdf71563112d31913fc1b6eb4bf008
KNitheshkumar <knithesh14397@gmail.com>
2023-09-29 12:21:16 +0530

    8th commit

--- csvtoyaml.py
+++ csvtoyaml.py
@@ -4,13 +4,12 @@ import yaml
 csv_file = "iit.csv"
 yaml_file = "output.yaml"
 
-
 data = []
 with open(csv_file, mode="r") as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         data.append(row)
-
+        
 output_data = []
 for row in data:
     item = {
@@ -23,6 +22,6 @@ for row in data:
     }
     output_data.append(item)
 with open(yaml_file, mode="w") as file:
-    yaml.dump(output_data, file, default_flow_style=False)
+    yaml.dump(output_data, file)
 
 print(f"CSV data has been converted to YAML and saved to {yaml_file}.")
